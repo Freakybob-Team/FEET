e243411e46236b802b5cb8f2472e6efbbadbff3c
Wish <100239673+wish13yt@users.noreply.github.com>
2025-01-12 11:38:11 -0600

    d

--- examples/1 - FRead Modified.py
+++ examples/1 - FRead Modified.py
@@ -1,14 +1,16 @@
 import json
 
 print("Freakifest Validator for FreakyBrowse")
-print("Warning! If you do not have F.json in the same directory, or its not set up properly, this will not work.")
-
-with open('F.fest', 'r') as file:
-    data = json.load(file)
-    print("Freakifest Version: " + data['f_ver'])
-    print("Name: " + data['name'])
-    print("Software Version: " + data['soft_ver'])
-    print("Description: " + data['description'])
-    print("Minimum FreakyBrowse Version: " + data['MinFBVersion'])
-    print("Success! ✅")
-    file.close()
+print("Warning! If you do not have F.json in the same directory, or its not set up properly, your Freakifest will not work.")
+try:
+    with open('F.fest', 'r') as file:
+        data = json.load(file)
+        print("Freakifest Version: " + data['f_ver'])
+        print("Name: " + data['name'])
+        print("Software Version: " + data['soft_ver'])
+        print("Description: " + data['description'])
+        print("Minimum FreakyBrowse Version: " + data['MinFBVersion'])
+        print("Success! ✅")
+        file.close()
+except:
+    print("Validation failed! ❌")
